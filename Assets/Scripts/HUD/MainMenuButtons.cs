2ac52874e1af82db8e1211b54528aecf1abac39e
Thingy25 <68366292+Thingy25@users.noreply.github.com>
2025-07-10 14:57:46 -0500

    Sprint 1 build setup

--- Assets/Scripts/HUD/MainMenuButtons.cs
+++ Assets/Scripts/HUD/MainMenuButtons.cs
@@ -6,6 +6,6 @@ public class MainMenuButtons : MonoBehaviour
 {
    public void LoadGameScene()
    {
-       SceneManager.LoadScene("PlayerControllerTest");
+       SceneManager.LoadScene("PuzzleTesting");
    }
 }
